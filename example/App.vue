eb0600ace593f3a94c0a6f27dbf3172f4c179d4c
jikeliuyang <18817843920@163.com>
2019-01-08 14:14:14 +0800

    rename class

--- example/App.vue
+++ example/App.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
-    <!-- <h1>Helllo World</h1>
-    <div style="color: red; font-size: 50px;margin-left: 100px">PX2REM</div>
-    <input type="text" placeholder="ni"> -->
-    <div class="easy-scss">
-    </div>
+    <div class="easy-css">2</div>
   </div>
-  
 </template>
 
 <script>
@@ -16,13 +11,13 @@
 </script>
 
 <style scoped>
-  .easy-scss{
+  .easy-css{
     border: 1px solid red;
+    posC;    
     wh(100px, 80px);
-    posC;
-    /* font(20px, yellow, center); */
-    /* comB(http://ww1.sinaimg.cn/large/b44313e1gy1fyz1li77jzj20q411wdop.jpg); */
-    /* flex(center, flex-start); */
+    font(20px, blue, center);
+    comB(http://ww1.sinaimg.cn/large/b44313e1gy1fyz1li77jzj20q411wdop.jpg);
+    /* flex(center, flex-start);*/
   }
 </style>
 
--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,6 @@ module.exports = {
       {
         test: /\.vue$/,
         use: [
-          //'vue-loader', 'easy-css-loader'
           {
             loader: 'vue-loader'
           },
